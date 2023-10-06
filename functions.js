f816cdccbdd30683c9a56a8c0c547c35bff08ae8
nadiapel <nadia.y.pelaez@gmail.com>
2023-10-06 18:23:58 -0400

    When the like button is pressed a second time the button changes to gray and the like is taken away. Can only like once.

--- functions.js
+++ functions.js
@@ -1,10 +1,18 @@
 function welcome() {
     document.getElementById("paragraph").innerHTML += "<br/>CSE312 Project"
 }
-
+var pressed = false
 
 function like() {
     var text = document.getElementById("like");
-    text.innerHTML = (parseInt(text.innerHTML) + 1).toString();
-    document.getElementById("like_button").style.backgroundColor = "LawnGreen";
+    if (pressed == false){
+        text.innerHTML = (parseInt(text.innerHTML) + 1).toString()
+        pressed = true
+        document.getElementById("like_button").style.backgroundColor = "LawnGreen";
+    }
+    else{
+        text.innerHTML = (parseInt(text.innerHTML) - 1).toString()
+        pressed = false
+        document.getElementById("like_button").style.backgroundColor = "LightGrey";
+    }
 }
