aa0ed05666179b578fb7d509cf18d3adef26acfd
jx <98677018+jxboulwa@users.noreply.github.com>
2023-11-04 14:39:21 -0400

    made a tempered objective 1 for working on objective 2
    update code.

--- server.py
+++ server.py
@@ -1,5 +1,3 @@
-from pymongo import MongoClient
-
 from util.request import Request
 import string
 from datetime import datetime, timezone, timedelta
@@ -9,56 +7,41 @@ import html
 import bcrypt
 import secrets
 import hashlib
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, make_response, request, session, copy_current_request_context
 import flask
 import os
 from flask import request, send_file
 from flask import Response
 from flask import render_template
 from markupsafe import escape
+from werkzeug.utils import secure_filename
+from database import OurDataBase
+from flask_socketio import SocketIO, emit, send
 
 app = Flask(__name__)
+app.secret_key = "secret key"
+UPLOAD_FOLDER = 'public/image/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins="*", transports=['websocket']) # Socket IO Initialization
+
 # for easy switching between local and docker
-clientname = "mongo"
-#clientname = "localhost"
-dbname = "cse312"
-
-
 def add_no_sniff(response):
     response.headers['X-Content-Type-Options'] = 'nosniff'
-
-
-class OurDataBase:
-    def __init__(self, client_name=clientname, db_name=dbname):
-        self.clientname = client_name
-        self.dbname = db_name
-        self.mongo_client = pymongo.MongoClient(client_name)
-        self.db = self.mongo_client[dbname]
-
-    def __getitem__(self, key):
-        return self.db[key]  # changed by Zuhra to be able to add a new collection before_it_was->["key"]
-
-    def close(self):
-        self.mongo_client.close()
-
-
+    
 def hash_token(token_as_string):
     return hashlib.sha256(token_as_string.encode()).hexdigest()
-
 
 def create_token(size=100):
     token = secrets.token_hex(size)
     return token
 
-
 def create_future_timestamp(secs=3600):
     toRet = datetime.now(timezone.utc) + timedelta(seconds=3600)
     return toRet.timestamp()
 
-
 def current_timestamp():
     return datetime.now(timezone.utc).timestamp()
-
 
 # checks if the user is authenticated if not or user is not logged in then returns None
 def user_authenticated():
@@ -71,7 +54,6 @@ def user_authenticated():
         if user is not None:
             return user["username"]
     return None
-
 
 @app.get("/")
 def site_root():
@@ -90,13 +72,11 @@ def site_root():
         resp.headers['X-Content-Type-Options'] = 'nosniff'
         return resp
 
-
 @app.route("/guest")
 def guest_login():
     resp = flask.redirect("/dashboard")
     add_no_sniff(resp)
     return resp
-
 
 @app.route("/dashboard")
 def dashboard():
@@ -108,7 +88,6 @@ def dashboard():
     resp.headers['Content-Type'] = 'text/html; charset=utf-8'
     resp.headers['X-Content-Type-Options'] = 'nosniff'
     return resp
-
 
 @app.get("/public/<file>")
 def send_static_file(file):
@@ -127,7 +106,6 @@ def send_static_file(file):
 
     if status != 200:
         return resp
-
     fileends = {"html": "text/html; charset = utf8",
                 "css": "text/css; charset = utf8",
                 "js": "text/javascript; charset = utf8",
@@ -137,9 +115,7 @@ def send_static_file(file):
     contenttype = file.split(".")[-1]
     if contenttype in fileends:
         resp.headers['Content-Type'] = fileends[contenttype]
-
     return resp
-
 
 @app.get("/public/image/<file>")
 def send_image_file(file):
@@ -158,7 +134,6 @@ def send_image_file(file):
 
     if status != 200:
         return resp
-
     fileends = {"html": "text/html; charset = utf8",
                 "css": "text/css; charset = utf8",
                 "js": "text/javascript; charset = utf8",
@@ -168,9 +143,7 @@ def send_image_file(file):
 
     if contenttype in fileends:
         resp.headers['Content-Type'] = fileends[contenttype]
-
     return resp
-
 
 @app.get("/visit-counter")
 def welcome_to_the_jungle():
@@ -192,7 +165,6 @@ def welcome_to_the_jungle():
     resp.headers['Content-Type'] = "text/plain"
     return resp
 
-
 @app.post("/register")
 def join_us_spongebob():
     req = flask.request
@@ -200,8 +172,7 @@ def join_us_spongebob():
     username = html.escape(req.form["username_reg"])
     password = req.form["password_reg"]
     db = OurDataBase()
-    users = db.__getitem__("Users")
-    # users = db["Users"]
+    users = db["Users"]
     found_user = users.find_one({"username": username})
     if found_user is None:
         bytess = password.encode('utf-8')
@@ -211,16 +182,13 @@ def join_us_spongebob():
         bodymessage = "Successfully created " + username + "!"
     else:
         bodymessage = username + " is already taken!"
-
     db.close()
     add_no_sniff(resp)
     resp.status = 301
     resp.data = bodymessage
     resp.headers['Content-Type'] = "text/plain"
     resp.headers['Location'] = "/"
-
     return resp
-
 
 @app.post("/login")
 def show_me_your_papers():
@@ -229,8 +197,7 @@ def show_me_your_papers():
     username = html.escape(req.form["username_login"])
     password = req.form["password_login"]
     db = OurDataBase()
-    users = db.__getitem__("Users")
-    # users = db["Users"]
+    users = db["Users"]
     found_user = users.find_one({"username": username})
     if found_user is not None:
         hashed_pw = found_user["password"]
@@ -250,7 +217,6 @@ def show_me_your_papers():
             resp.headers['Location'] = "/"
     else:
         bodymessage = "Incorrect username or password"
-
     db.close()
     add_no_sniff(resp)
     resp.status = 301
@@ -259,47 +225,43 @@ def show_me_your_papers():
     resp.headers['Location'] = "/"
     return resp
 
-
 @app.get("/get-username")
 def get_username():
-    db = OurDataBase()
     auth_token = flask.request.cookies.get('token')
     if auth_token is not None:
+        db = OurDataBase()
+        users = db["Users"]
         token_hash = hash_token(auth_token)
-        users = db.__getitem__("Users")
-        # users = db["Users"]
         user = users.find_one({"token": token_hash})
         if user is not None:
-            username = str(user["username"])
-            username = username.replace("&lt;", "<")
-            username = username.replace("&gt;", ">")
-            if username.find("&amp"):
-                username = username.replace("&amp;", "&")
+            username = escape(user["username"])
             response = jsonify({"username": username})
             response.headers['X-Content-Type-Options'] = 'nosniff'
             return response, 200
-
     response = jsonify({"username": "Guest"})
     response.headers['X-Content-Type-Options'] = 'nosniff'
     return response, 200
-
 
 @app.post("/create-post")
 def create_post():
     req = flask.request
     resp = flask.Response()
-
     # Check if user is authenticated (has a valid token)
     auth_token = req.cookies.get('token')
     if auth_token is None:
         resp.status = 401  # Unauthorized
         resp.data = "You need to be logged in to create a post."
         return resp
-
     # Retrieve title and description from the form
     title = html.escape(req.form.get("title"))
+    answer_method = html.escape(req.form.get("answer_method"))
+    correct_answers = html.escape(req.form.get("correct_answers"))
     description = html.escape(req.form.get("description"))
-
+    image = req.files['file']
+    if not title or not description:
+        resp.status = 400  # Bad request
+        resp.data = "Title and description are required."
+        return resp
     # Get the username of the authenticated user
     db = OurDataBase()
     users = db["Users"]
@@ -310,38 +272,62 @@ def create_post():
         resp.status = 401  # Unauthorized
         resp.data = "Invalid token. Please log in."
         return resp
-
     username = user["username"]
+    posts = db["Posts"]
+    # create a unique post ID
+    post_id = str(posts.count_documents({}) + 1)
+    # add post to database
+    image_path=""
+    if image.filename != '':
+        filename = secure_filename(image.filename)
+        image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        image_path="public/image/"+filename
+        print(image_path)
+    if answer_method == "text":
+        posts.insert_one({
+            "title": title,
+            "description": description,
+            "username": username,
+            "post_id": post_id,
+            "answer_method": answer_method,
+            "correct_answers" : correct_answers,
+            "image_filename":image_path,
+            "time" : datetime.now(),
+            "submited_users" :""
+        })
+        db.close()
+    else:
+        choice1= html.escape(req.form.get("choice1"))
+        choice2 = html.escape(req.form.get("choice2"))
+        choice3 = html.escape(req.form.get("choice3"))
+        choice4 = html.escape(req.form.get("choice4"))
+        posts.insert_one({
+            "title": title,
+            "description": description,
+            "username": username,
+            "post_id": post_id,
+            "answer_method": answer_method,
+            "correct_answers" : correct_answers,
+            "choice1":choice1,
+            "choice2":choice2,
+            "choice3":choice3,
+            "choice4":choice4,
+            "image_filename":image_path,
+            "time" : datetime.now(),
+            "submited_users" :""
+        })
+        db.close()
+        
+    # Socket IO Events - New Post Creation
+    @socketio.on('connect', namespace='/posts')
+    def handle_connect():
+        auth_token = request.cookies.get('token')
+        if auth_token is None:
+            socketio.disconnect()
+            return
+        #retrieve the username from the Flask-SocketIO session
 
-    # old one - juila
-    # Save the post to the database
-    # posts = db["Posts"]
 
-    # Added by zuhra to create a new collection
-    posts = db.__getitem__("Posts")
-
-    # creating unique ids for posts
-    collection = posts.find({})
-    id_post = 1
-    like_count = "0"
-    for i in collection:
-        id_post = int(i.get('post_id', 0)) + 1
-
-    # added likes and unique post ID
-    posts.insert_one({
-        "title": title,
-        "description": description,
-        "username": username,
-        "total": like_count,
-        "post_id": str(id_post)
-    })
-
-    # print db collection for posts
-    collection = posts.find({})
-    for i in collection:
-        print(i)
-
-    db.close()
 
     # Redirect to the home page after creating the post
     resp.status = 302
@@ -350,57 +336,18 @@ def create_post():
     return resp
 
 
+
+
+
+
 @app.route("/chat-history", methods=["GET"])
 def chat_history():
     db = OurDataBase()
-    posts = db.__getitem__("Posts")
-    existing_posts = list(posts.find({}, {'_id': 0}))
+    posts = db["Posts"]
+    existing_posts = list(posts.find({}, {"_id": 0}))
+    db.close()
+    return jsonify(existing_posts)
 
-    response = app.make_response(existing_posts)
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    return response
-
-# Like for Obj3
-@app.route("/like", methods=["POST"])
-def like():
-    data = request.get_json()
-    current_like = data.get('like')
-    post_id = data.get('post_id')
-    # total = int(data.get('total'))
-    response = flask.Response()
-    db = OurDataBase()
-    liked_status = db.__getitem__("Status")
-    posts = db.__getitem__("Posts")
-    total = posts.find_one({"post_id": post_id})
-    total = int(total["total"])
-
-    user = user_authenticated()
-    if user is not None:
-        status = liked_status.find_one({"username": user, "post_id": post_id})
-        if status is None:
-            liked_status.insert_one({"username": user, "status": "1", "post_id": post_id})
-            status = liked_status.find_one({"username": user, "post_id": post_id})
-
-        if current_like == "0" and status["status"] != "1":
-            posts.update_one({"post_id": post_id}, {"$set": {"total": str(total - 1)}})
-            liked_status.update_one({"username": user, "post_id": post_id}, {"$set": {"status": "1"}})
-
-        elif current_like == "1" and status["status"] != "0":
-            posts.update_one({"post_id": post_id}, {"$set": {"total": str(total + 1)}})
-            liked_status.update_one({"username": user, "post_id": post_id}, {"$set": {"status": "0"}})
-
-        elif current_like == "0" and status["status"] == "1":
-            posts.update_one({"post_id": post_id}, {"$set": {"total": str(total + 1)}})
-            liked_status.update_one({"username": user, "post_id": post_id}, {"$set": {"status": "0"}})
-
-        elif current_like == "1" and status["status"] == "0":
-            posts.update_one({"post_id": post_id}, {"$set": {"total": str(total - 1)}})
-            liked_status.update_one({"username": user, "post_id": post_id}, {"$set": {"status": "1"}})
-
-    response.status = 302
-    response.headers['Location'] = "/"
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    return response
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=False)
+   app.run(host="0.0.0.0", port=8080, debug=False)
