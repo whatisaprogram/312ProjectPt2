486c09443181151f4a0a1520c81ffa4901287287
zmsaleh <85408892+zmsaleh@users.noreply.github.com>
2023-11-13 03:10:47 -0500

    not sure if done correctly

---
+++ 312ProjectPt2-main 2/README.md
@@ -0,0 +1,11 @@
+# 312ProjectPt2
+
+This is a readme file, pending updates. This readme will describe how to load the server in.
+
+## Getting started
+
+run "docker-compose up --build --force-recreate" in the command prompt. This will start the server and the database.
+
+Most of the frontend is directly taken from the HW2 frontend, for now.
+
+Objective 1 is implemented with the flask framework and mongoDB. 
---
+++ 312ProjectPt2-main 2/util/increment_question_id.py
@@ -0,0 +1,34 @@
+import pymongo
+
+
+def increment_question_id(a_db_instance):
+    question_ids = a_db_instance["question_ids"]
+    found = question_ids.find_one()
+    if found is None:
+        question_ids.insert_one({"id": 1})
+        return 1
+    else:
+        current_id = found["id"]
+        newvalues = {"$set": {"id": current_id + 1}}
+        question_ids.update_one({"id": current_id}, newvalues)
+        return current_id + 1
+
+
+def add_question(a_db_instance, a_dictionary, username):
+    questions = a_db_instance["questions"]
+    new_id = increment_question_id(a_db_instance)
+    toinsert = {"id": new_id, "username": "username", "title": a_dictionary["title"],
+                "description": a_dictionary["description"],
+                "method": a_dictionary["method"], "answers": a_dictionary["answers"], "imgurl": a_dictionary["imgurl"]}
+    questions.insert_one(toinsert)
+    return new_id
+
+
+def get_all_questions(db):
+    questions = db["Posts"]
+    qs = questions.find()
+    all_questions = []
+    for q in qs:
+        all_questions.append(q)
+    print(all_questions)
+    return all_questions
